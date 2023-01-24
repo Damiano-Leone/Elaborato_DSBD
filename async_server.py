cb7b93407e62eaa754ac1b8844fd39120873f5f3
Damiano <damy.leone@gmail.com>
2023-01-24 16:57:45 +0100

    Modified

--- async_server.py
+++ async_server.py
@@ -27,41 +27,75 @@ class RetrievalService(retrieval_pb2_grpc.RetrievalServiceServicer):
         except sql.Error as e:
             print(f"Error connecting to Mysql DB: {e}")
             sys.exit(1)
+    
+    
+    def __del__(self) -> None:
+        self.cnx.close()
         
         
     async def GetDickeyFuller(
             self, request: retrieval_pb2.RetrievalRequest,
             context: grpc.aio.ServicerContext) -> retrieval_pb2.RetrievalDickeyFuller:
-        for result in self.cursor.execute(request.query):
-            return result.fetchall()
+        self.cursor.execute(request.query)
+        for result in self.cursor.fetchall():
+            return retrieval_pb2.RetrievalDickeyFuller(
+                    name=result[1], partition=result[2], test_statistic=result[3], 
+                    p_value=result[4], is_stationary=result[5], crit_value1=result[6],
+                    crit_value5=result[7], crit_value10=result[8])
+        return retrieval_pb2.RetrievalDickeyFuller(
+            name='', partition='',test_statistic=0, 
+            p_value=0, is_stationary=0, crit_value1=0,
+            crit_value5=0, crit_value10=0)
     
     
     async def ListDecomposition(
             self, request:retrieval_pb2.RetrievalRequest,
             context: grpc.aio.ServicerContext) -> AsyncIterable[retrieval_pb2.RetrievalDecomposition]:
-        for result in self.cursor.execute(request.query):
-            yield result.fetchall()
+        self.cursor.execute(request.query)
+        for result in self.cursor.fetchall():
+            yield retrieval_pb2.RetrievalDecomposition(
+                    name=result[1], partition=result[2], timestamp=result[3].strftime('%d/%m/%Y, %H:%M:%S'), 
+                    typology=result[4], value=result[5])
+        yield retrieval_pb2.RetrievalDecomposition(
+            name='', partition='', timestamp='', 
+            typology='', value=0)
             
             
     async def ListHodrickPrescott(
             self, request:retrieval_pb2.RetrievalRequest,
             context: grpc.aio.ServicerContext) -> AsyncIterable[retrieval_pb2.RetrievalHodrickPrescott]:
-        for result in self.cursor.execute(request.query):
-            yield result.fetchall()
+        self.cursor.execute(request.query)
+        for result in self.cursor.fetchall():
+            yield retrieval_pb2.RetrievalHodrickPrescott(
+                    name=result[1], partition=result[2], timestamp=result[3].strftime('%d/%m/%Y, %H:%M:%S'), 
+                    value=result[4])
+        yield retrieval_pb2.RetrievalHodrickPrescott(
+                name='', partition='', timestamp='', 
+                value=0)
             
             
     async def ListAutocorrelation(
             self, request:retrieval_pb2.RetrievalRequest,
             context: grpc.aio.ServicerContext) -> AsyncIterable[retrieval_pb2.RetrievalAutocorrelation]:
-        for result in self.cursor.execute(request.query):
-            yield result.fetchall()
+        self.cursor.execute(request.query)
+        for result in self.cursor.fetchall():
+            yield retrieval_pb2.RetrievalAutocorrelation(
+                    name=result[1], partition=result[2], value=result[3])
+        yield retrieval_pb2.RetrievalAutocorrelation(
+                name='', partition='', value=0)
             
             
     async def ListAggregates(
             self, request:retrieval_pb2.RetrievalRequest,
             context: grpc.aio.ServicerContext) -> AsyncIterable[retrieval_pb2.RetrievalAggregates]:
-        for result in self.cursor.execute(request.query):
-            yield result.fetchall() 
+        self.cursor.execute(request.query)
+        for result in self.cursor.fetchall():
+            yield retrieval_pb2.RetrievalAggregates(
+                    name=result[1], partition=result[2], range=result[3],
+                    typology=result[4], value=result[5], predicted_value=result[6])
+        yield retrieval_pb2.RetrievalAggregates(
+                name='', partition='', range='', 
+                typology='', value=0, predicted_value=0)
 
 
 
--- asynch_client.py
+++ asynch_client.py
@@ -5,70 +5,61 @@ import grpc
 import retrieval_pb2
 import retrieval_pb2_grpc
 
+import sys
 
 
 # Performs an unary call
 async def get_dickey_fuller(stub: retrieval_pb2_grpc.RetrievalServiceStub,
                             request: retrieval_pb2.RetrievalRequest) -> None:
     result = await stub.GetDickeyFuller(request)
-    if not result:
+    if not result.name:
         print("No rows found.\n\n")
     else:
-        print(f"Query found:\n {result.name} {result.partition} {result.test_statistic} {result.p_value} {result.is_stationary} {result.crit_value1} {result.crit_value5} {result.crit_value10}\n\n")
+        print("Row found:\n")
+        print(f"| nome metrica: {result.name} | partizione: {result.partition} | test_statistic: {round(result.test_statistic, 3)} | p_value: {round(result.p_value, 3)} | is_stationary: {result.is_stationary} | crit_value_1: {round(result.crit_value1, 3)} | crit_value_5: {round(result.crit_value5, 3)} | crit_value_10: {round(result.crit_value10, 3)} |")  
+        print("\n\n")
         
-
+        
 # Performs server-streaming calls
 async def list_decomposition(stub: retrieval_pb2_grpc.RetrievalServiceStub,
                              request: retrieval_pb2.RetrievalRequest) -> None:
     results = stub.ListDecomposition(request)
-    if not results:
-        print("No rows found.\n\n")
-    else:
-        print("Rows found:\n")
-        async for result in results:
-            print(f" {result.name} {result.partition} {result.timestamp} {result.typology} {result.value}\n")
-        print("\n")
+    async for result in results:
+        if result.name:
+            print(f"| nome metrica: {result.name} | partizione: {result.partition} | timestamp: {result.timestamp} | tipologia: {result.typology} | value: {round(result.value, 1)} |")
+    print("\n")
+                
         
-
 async def list_hodrick_prescott(stub: retrieval_pb2_grpc.RetrievalServiceStub,
                              request: retrieval_pb2.RetrievalRequest) -> None:
     results = stub.ListHodrickPrescott(request)
-    if not results:
-        print("No rows found.\n\n")
-    else:
-        print("Rows found:\n")
-        async for result in results:
-            print(f" {result.name} {result.partition} {result.timestamp} {result.value}\n")
-        print("\n")
+    async for result in results:
+        if result.name:
+            print(f"| nome metrica: {result.name} | partizione: {result.partition} | timestamp: {result.timestamp} | value: {round(result.value, 1)} |")
+    print("\n")
         
 
 async def list_autocorrelation(stub: retrieval_pb2_grpc.RetrievalServiceStub,
                              request: retrieval_pb2.RetrievalRequest) -> None:
     results = stub.ListAutocorrelation(request)
-    if not results:
-        print("No rows found.\n\n")
-    else:
-        print("Rows found:\n")
-        async for result in results:
-            print(f" {result.name} {result.partition} {result.value}\n")
-        print("\n")
+    async for result in results:
+        if result.name:
+            print(f"| nome metrica: {result.name} | partizione: {result.partition} | value: {round(result.value, 1)} |")
+    print("\n")
         
         
 async def list_aggregates(stub: retrieval_pb2_grpc.RetrievalServiceStub,
                              request: retrieval_pb2.RetrievalRequest) -> None:
     results = stub.ListAggregates(request)
-    if not results:
-        print("No rows found.\n\n")
-    else:
-        print("Rows found:\n")
-        async for result in results:
-            print(f" {result.name} {result.partition} {result.range} {result.typology} {result.value} {result.predicted_value}\n")
-        print("\n")
+    async for result in results:
+        if result.name:
+            print(f"| nome metrica: {result.name} | partizione: {result.partition} | range: {result.range} | tipologia: {result.typology} | value: {round(result.value, 1)} | valore predetto: {round(result.predicted_value, 1)} |")
+    print("\n")
 
 
 #Metriche:
 #Metrica2: node_memory_MemFree_bytes
-#Metrica3: node_memory_MemAvailable_bytes 
+#Metrica3: node_memory_memAvail_bytes 
 #Metrica4: node_filesystem_free_bytes(2-partition: /dev/sda2, tmpfs)
 #Metrica5: node_filesystem_avail_bytes(2-partition: /dev/sda2, tmpfs)
 #Metrica7: node_filefd_allocated
@@ -79,7 +70,7 @@ async def node_memory_MemFree_bytes(stub: retrieval_pb2_grpc.RetrievalServiceStu
     request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="memory_MemFree_bytes"')
     print("-------------- GetDickeyFuller --------------\n")
     await get_dickey_fuller(stub, request)
-                
+        
     request = retrieval_pb2.RetrievalRequest(query='select * from decomposition where nome="memory_MemFree_bytes"')
     print("-------------- ListDecomposition --------------\n")
     await list_decomposition(stub, request)
@@ -100,33 +91,33 @@ async def node_memory_MemFree_bytes(stub: retrieval_pb2_grpc.RetrievalServiceStu
 
 
 async def node_memory_MemAvailable_bytes(stub: retrieval_pb2_grpc.RetrievalServiceStub) -> None:
-    print( "Metrica 2: node_memory_MemAvailable_bytes\n")
+    print( "Metrica 2: node_memory_memAvail_bytes\n")
                 
-    request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="memory_MemAvailable_bytes"')
+    request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="memory_memAvail_bytes"')
     print("-------------- GetDickeyFuller --------------\n")
     await get_dickey_fuller(stub, request)
                 
-    request = retrieval_pb2.RetrievalRequest(query='select * from decomposition where nome="memory_MemAvailable_bytes"')
+    request = retrieval_pb2.RetrievalRequest(query='select * from decomposition where nome="memory_memAvail_bytes"')
     print("-------------- ListDecomposition --------------\n")
     await list_decomposition(stub, request)
                 
-    request = retrieval_pb2.RetrievalRequest(query='select * from hodrick_prescott where nome="memory_MemAvailable_bytes"')
+    request = retrieval_pb2.RetrievalRequest(query='select * from hodrick_prescott where nome="memory_memAvail_bytes"')
     print("-------------- ListHodrickPrescott --------------\n")
     await list_hodrick_prescott(stub, request)
                 
-    request = retrieval_pb2.RetrievalRequest(query='select * from autocorrelation where nome="memory_MemAvailable_bytes"')
+    request = retrieval_pb2.RetrievalRequest(query='select * from autocorrelation where nome="memory_memAvail_bytes"')
     print("-------------- ListAutocorrelation --------------\n")
     await list_autocorrelation(stub, request)
                 
-    request = retrieval_pb2.RetrievalRequest(query='select * from aggregates where nome="memory_MemAvailable_bytes"')
+    request = retrieval_pb2.RetrievalRequest(query='select * from aggregates where nome="memory_memAvail_bytes"')
     print("-------------- ListAggregates --------------\n")
     await list_aggregates(stub, request)
     
-    print( "Fine stampa Metrica 2: node_memory_MemAvailable_bytes\n")
+    print( "Fine stampa Metrica 2: node_memory_memAvail_bytes\n")
     
     
 async def node_filesystem_free_bytes_dev_sda2(stub: retrieval_pb2_grpc.RetrievalServiceStub) -> None:
-        print( "Metrica 3: node_filesystem_free_bytes\nPartizione: /dev/sda2\n")
+        print( "Metrica 3: node_filesystem_free_bytes  Partizione: /dev/sda2\n")
                 
         request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="filesystem_free_bytes" and partizione="/dev/sda2"')
         print("-------------- GetDickeyFuller --------------\n")
@@ -148,11 +139,11 @@ async def node_filesystem_free_bytes_dev_sda2(stub: retrieval_pb2_grpc.Retrieval
         print("-------------- ListAggregates --------------\n")
         await list_aggregates(stub, request)
         
-        print( "Fine stampa Metrica 3: node_filesystem_free_bytes\nPartizione: /dev/sda2\n")
+        print( "Fine stampa Metrica 3: node_filesystem_free_bytes  Partizione: /dev/sda2\n")
 
 
 async def node_filesystem_free_bytes_tmpfs(stub: retrieval_pb2_grpc.RetrievalServiceStub) -> None:
-        print( "Metrica 3: node_filesystem_free_bytes\nPartizione: tmpfs\n")
+        print( "Metrica 3: node_filesystem_free_bytes  Partizione: tmpfs\n")
                 
         request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="filesystem_free_bytes" and partizione="tmpfs"')
         print("-------------- GetDickeyFuller --------------\n")
@@ -174,11 +165,11 @@ async def node_filesystem_free_bytes_tmpfs(stub: retrieval_pb2_grpc.RetrievalSer
         print("-------------- ListAggregates --------------\n")
         await list_aggregates(stub, request)
         
-        print( "Fine stampa Metrica 3: node_filesystem_free_bytes\nPartizione: tmpfs\n")
+        print( "Fine stampa Metrica 3: node_filesystem_free_bytes  Partizione: tmpfs\n")
 
 
 async def node_filesystem_avail_bytes_dev_sda2(stub: retrieval_pb2_grpc.RetrievalServiceStub) -> None:
-    print( "Metrica 4: node_filesystem_avail_bytes\nPartizione: /dev/sda2\n")
+    print( "Metrica 4: node_filesystem_avail_bytes  Partizione: /dev/sda2\n")
                 
     request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="filesystem_avail_bytes" and partizione="/dev/sda2"')
     print("-------------- GetDickeyFuller --------------\n")
@@ -200,11 +191,11 @@ async def node_filesystem_avail_bytes_dev_sda2(stub: retrieval_pb2_grpc.Retrieva
     print("-------------- ListAggregates --------------\n")
     await list_aggregates(stub, request)
     
-    print( "Fine stampa Metrica 4: node_filesystem_avail_bytes\nPartizione: /dev/sda2\n")
+    print( "Fine stampa Metrica 4: node_filesystem_avail_bytes  Partizione: /dev/sda2\n")
 
 
 async def node_filesystem_avail_bytes_tmpfs(stub: retrieval_pb2_grpc.RetrievalServiceStub) -> None:
-    print( "Metrica 4: node_filesystem_avail_bytes\nPartizione: tmpfs\n")
+    print( "Metrica 4: node_filesystem_avail_bytes  Partizione: tmpfs\n")
                 
     request = retrieval_pb2.RetrievalRequest(query='select * from dickey_fuller where nome="filesystem_avail_bytes" and partizione="tmpfs"')
     print("-------------- GetDickeyFuller --------------\n")
@@ -226,7 +217,7 @@ async def node_filesystem_avail_bytes_tmpfs(stub: retrieval_pb2_grpc.RetrievalSe
     print("-------------- ListAggregates --------------\n")
     await list_aggregates(stub, request)
     
-    print( "Fine stampa Metrica 4: node_filesystem_avail_bytes\nPartizione: tmpfs\n")
+    print( "Fine stampa Metrica 4: node_filesystem_avail_bytes  Partizione: tmpfs\n")
 
 
 async def node_filefd_allocated(stub: retrieval_pb2_grpc.RetrievalServiceStub) -> None:
@@ -257,47 +248,49 @@ async def node_filefd_allocated(stub: retrieval_pb2_grpc.RetrievalServiceStub) -
 
 
 async def run() -> None:
-    print("Sending...")
     async with grpc.aio.insecure_channel('localhost:50051') as channel:
         stub = retrieval_pb2_grpc.RetrievalServiceStub(channel)
-    
-    while(True):
-        user_input = int(input("Inserisci un numero intero corrispondente a una metrica:\n\n1-node_memory_MemFree_bytes\n2-node_memory_MemAvailable_bytes\n3-node_filesystem_free_bytes\n4-node_filesystem_avail_bytes\n5-node_filefd_allocated\n6-Richiedi i dati relativi a tutte le metriche\n>>"))
+        
+        while(True):
+            user_input = int(input("Inserisci un numero intero corrispondente a una metrica:\n\n1-node_memory_MemFree_bytes\n2-node_memory_memAvail_bytes\n3-node_filesystem_free_bytes\n4-node_filesystem_avail_bytes\n5-node_filefd_allocated\n6-Richiedi i dati relativi a tutte le metriche\n7-Exit\n>>"))
 
-        match user_input:
+            match user_input:
 
-            case 1:
-                await node_memory_MemFree_bytes(stub)
+                case 1:
+                    await node_memory_MemFree_bytes(stub)
                 
-            case 2:
-                await node_memory_MemAvailable_bytes(stub)
+                case 2:
+                    await node_memory_MemAvailable_bytes(stub)
                 
-            case 3:
-                await node_filesystem_free_bytes_dev_sda2(stub)
-                await node_filesystem_free_bytes_tmpfs(stub)
+                case 3:
+                    await node_filesystem_free_bytes_dev_sda2(stub)
+                    await node_filesystem_free_bytes_tmpfs(stub)
                 
-            case 4:
-                await node_filesystem_avail_bytes_dev_sda2(stub)
-                await node_filesystem_avail_bytes_tmpfs(stub)
+                case 4:
+                    await node_filesystem_avail_bytes_dev_sda2(stub)
+                    await node_filesystem_avail_bytes_tmpfs(stub)
                 
-            case 5:
-                await node_filefd_allocated(stub)
+                case 5:
+                    await node_filefd_allocated(stub)
                 
-            case 6:
-                await node_memory_MemFree_bytes(stub)
-                await node_memory_MemAvailable_bytes(stub)
-                await node_filesystem_free_bytes_dev_sda2(stub)
-                await node_filesystem_free_bytes_tmpfs(stub)
-                await node_filesystem_avail_bytes_dev_sda2(stub)
-                await node_filesystem_avail_bytes_tmpfs(stub)
-                await node_filefd_allocated(stub)
+                case 6:
+                    await node_memory_MemFree_bytes(stub)
+                    await node_memory_MemAvailable_bytes(stub)
+                    await node_filesystem_free_bytes_dev_sda2(stub)
+                    await node_filesystem_free_bytes_tmpfs(stub)
+                    await node_filesystem_avail_bytes_dev_sda2(stub)
+                    await node_filesystem_avail_bytes_tmpfs(stub)
+                    await node_filefd_allocated(stub)
                 
-            case _:
-                print( "Errore! Inserire un numero intero compreso tra 1 e 6.\n")
+                case 7:
+                    sys.exit(0)
+                
+                case _:
+                    print( "Errore! Inserire un numero intero compreso tra 1 e 7.\n")
 
 
 #Qualora si dovesse estendere il microservizio per fare anche altro parallelamente oltre alle richieste,
-#inserire il codice all'interno di other().
+#inserire il codice all'interno di other():
 async def other():
     for i in range(25):
         await asyncio.sleep(1)
